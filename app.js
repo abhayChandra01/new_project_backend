09b455eeb3e9151503196ae10b62c701079c4025
Abhay Chandra <a4abhaychandra@gmail.com>
2023-03-19 00:07:11 +0530

    db setup

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const db = require('./routes/db');
+
+db();
+
 var app = express();
 
 // view engine setup
